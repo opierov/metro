04d7feb73410f3adc56e07820f4cf13e1efb2a1f
opierov <oleksandr.pierov@gmail.com>
2025-01-10 14:55:29 +0100

    Update main on main branch

--- src/main/java/com/solvd/metro/Main.java
+++ src/main/java/com/solvd/metro/Main.java
@@ -449,6 +449,14 @@ public class Main {
         }
 
         logger.info("All threads completed");
+
+        passengers.add(new Passenger(20, new Ticket(101, LocalDateTime.now(), new BigDecimal("5.50"), null)));
+        passengers.add(new Passenger(31, null));
+        passengers.add(new Passenger(42, new Ticket(102, LocalDateTime.now(), new BigDecimal("8.00"), null)));
+
+
+
+
     }
 
 }
